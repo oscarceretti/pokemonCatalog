d485548dcb7144332827801859e93117f3c39dec
Oscar <o.ceretti@reply.it>
2021-02-09 21:18:37 +0100

    remove main storyboard and sceneDelegate

--- PokemonCatalog/PokemonCatalog/ViewController.swift
+++ PokemonCatalog/PokemonCatalog/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        debugPrint("EUREKA")
+        self.view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
