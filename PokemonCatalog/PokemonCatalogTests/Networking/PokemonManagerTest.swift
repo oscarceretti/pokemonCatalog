782d25bade1d88aded49965e75175717297b0a1e
Oscar <o.ceretti@reply.it>
2021-02-19 17:50:45 +0100

    add test for interactors

---
+++ PokemonCatalog/PokemonCatalogTests/Networking/PokemonManagerTest.swift
@@ -0,0 +1,40 @@
+//
+//  PokemonManagerTest.swift
+//  PokemonCatalogTests
+//
+//  Created by Oscar Ceretti on 19/02/2021.
+//
+
+import XCTest
+@testable import PokemonCatalog
+class PokemonInteractorTest: XCTestCase {
+    
+    var sut: PokemonListInteractor!
+    
+    override func setUp() {
+        super.setUp()
+        sut = PokemonListInteractor(dependencies: AppDependencies())
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_get_list() {
+        var receivedPokemons: [PokemonEntity]? = nil
+        var receivedError: String? = nil
+        
+        sut.getPokemonEntity { (pokemons, error) in
+            receivedPokemons = pokemons
+            receivedError = error
+            XCTAssertTrue(!receivedPokemons!.isEmpty)
+            XCTAssertNil(receivedError)
+        }
+  
+    }
+   
+    
+}
+
+
