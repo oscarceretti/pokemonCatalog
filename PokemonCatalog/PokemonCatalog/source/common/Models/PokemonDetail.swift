958761fdce0b1fad0fa4fa47756266fa1e5f26c4
Oscar <o.ceretti@reply.it>
2021-03-19 13:46:23 +0100

    remove unused code

--- PokemonCatalog/PokemonCatalog/source/common/Models/PokemonDetail.swift
+++ PokemonCatalog/PokemonCatalog/source/common/Models/PokemonDetail.swift
@@ -6,11 +6,6 @@
 //
 
 
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let pokemonDetail = try? newJSONDecoder().decode(PokemonDetail.self, from: jsonData)
-
 import Foundation
 
 // MARK: - PokemonDetail
